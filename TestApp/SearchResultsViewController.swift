5d7b905b5370237051289e0516d072af06f90f91
zhang <zhang@zhang-MacbookPro.local>
2014-06-18 15:26:40 +0800

    2014.06.18 更新ViewController到SearchResultsViewController

--- TestApp/SearchResultsViewController.swift
+++ TestApp/SearchResultsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchResultsViewController.swift
 //  TestApp
 //
 //  Created by zhang on 14-6-13.
@@ -7,7 +7,7 @@
 //
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
+class SearchResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
     
     
     // TabView object
